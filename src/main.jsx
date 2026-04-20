9cbf4d53d63def841297561e4a4f97a563d554b9
willieson <michaelwillieson@gmail.com>
2026-04-20 22:55:01 +0700

    analystic speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Pastikan import ini ada
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+
+// UBAH: Gunakan /react untuk proyek Vite/React biasa
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"; // Opsional: Untuk pantau jumlah pengunjung
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      {/* Tambahkan di sini agar aktif di seluruh rute */}
+      <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 );
